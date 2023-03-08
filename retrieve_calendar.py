3154132c4c63cb60fa8aff4f3c924447f6ff4350
Arnav Shirodkar <66931126+Arnavkar@users.noreply.github.com>
2023-03-08 02:24:07 -0500

    Added requirement.txt, made working version of free time slot identifier (brute force)

--- retrieve_calendar.py
+++ retrieve_calendar.py
@@ -3,71 +3,85 @@ from googleapiclient.errors import HttpError
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from datetime import datetime, timedelta
+from google.auth.transport.requests import Request
 import pytz
+import os
 
-# Define the scopes for the Google Calendar API
-scopes = ['https://www.googleapis.com/auth/calendar.readonly']
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
-# Set up the credentials flow
-flow = InstalledAppFlow.from_client_secrets_file('secret.json', scopes)
+def authenticate_user(calendar_id):
 
-# Obtain the user's authorization
-creds = flow.run_local_server(port=0)
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists(f'token_{calendar_id}.json'):
+        creds = Credentials.from_authorized_user_file(f'token_{calendar_id}.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'secret.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open(f'token_{calendar_id}.json', 'w') as token:
+            token.write(creds.to_json())
+    assert(creds!=None)
+    return creds
 
-calendar_ids = ['as9086@bard.edu', 'hm1795@bard.edu']
 
-# Set up the Calendar API service object
-service = build('calendar', 'v3', credentials=creds)
+def get_busy_times_google(creds,calendar_id,start_time,end_time):
+    # Define the scopes for the Google Calendar API
+    try:
+        # Set up the Calendar API service object
+        service = build('calendar', 'v3', credentials=creds)
 
-# Define the start and end times for the time range you want to check for free slots
-start_time = datetime.now(pytz.utc)
-end_time = start_time + timedelta(days=7)
+        # Define the request body for the free/busy query
+        request_body = {
+            'timeMin': start_time.isoformat(),
+            'timeMax': end_time.isoformat(),
+            'items': [{'id': calendar_id}],
+            'timeZone':"EST"
+        }
+        # Call the free/busy query API
+        freebusy = service.freebusy().query(body=request_body).execute()
+        # Extract the free/busy information from the response
+        calendars = freebusy['calendars']
+        busy_time_list = calendars[calendar_id]['busy']
+        return busy_time_list
 
-# Define the time range in the format expected by the free/busy query
-time_range = {
-    'timeMin': start_time.isoformat(),
-    'timeMax': end_time.isoformat(),
-    'timeZone': 'UTC',
-}
+    except HttpError as error:
+        print(f'An error occurred: {error}')
 
-# Define the request body for the free/busy query
-request_body = {
-    'timeMin': start_time.isoformat(),
-    'timeMax': end_time.isoformat(),
-    'timeZone': 'UTC',
-    'items': [{'id': calendar_id} for calendar_id in calendar_ids],
-    'calendarExpansionMax': len(calendar_ids),
-    'groupExpansionMax': len(calendar_ids),
-    'freeBusyView': 'BUSY',
-}
+def compute_free_times(busy_times,start_time,end_time):
+    # Identify the free time slots
+    busy_times.sort()
+    free_periods = []
+    last_end = start_time
+    for busy_start, busy_end in busy_times:
+        if last_end < busy_start:
+            free_periods.append((last_end, busy_start))
+        last_end = max(last_end, busy_end)
+    if last_end < end_time:
+        free_periods.append((last_end, end_time))
 
-try:
-    # Call the free/busy query API
-    freebusy = service.freebusy().query(body=request_body).execute()
-    print(freebusy)
-    # Extract the free/busy information from the response
-    calendars = freebusy['calendars']
-    busy_times = [set(calendars[calendar_id]['busy']) for calendar_id in calendars]
+    print("FREE TIMES-------")
+    for free_start, free_end in free_periods:
+        print(f'{free_start.strftime("%x - %X")} to {free_end.strftime("%x - %X")}')
 
-    # Compute the free time slots by finding the intersection of the busy times
-    # and the time range, and then subtracting the busy times from the time range
-    free_times = set()
-    for i in range(int((end_time-start_time).total_seconds()/1800)):
-        free_times.add(start_time + timedelta(minutes=30)*i)
-
-    print("BUSY TIMES-----")
-    # print(busy_times)
-
-    print("FREE TIMES-----")
-    # print(free_times)
-
-    for busy_set in busy_times:
-        free_times = free_times - busy_set
-
-    # Print the free time slots
-    # print("Free Time Slots:")
-    # for free_time in sorted(list(free_times)):
-    #     print(free_time.astimezone(pytz.timezone('US/Pacific')).strftime('%m/%d/%Y %I:%M %p %Z'))
-
-except HttpError as error:
-    print(f'An error occurred: {error}')
+if __name__ == '__main__':
+    calendar_ids = ['as9086@bard.edu', 'arnavshirodkar@gmail.com']
+    tz = pytz.timezone("EST")
+    start_time = datetime.now(tz)
+    end_time = start_time + timedelta(days=1)
+    busy_times = []
+    for calendar_id in calendar_ids:
+        creds = authenticate_user(calendar_id)
+        for busy_time in get_busy_times_google(creds,calendar_id,start_time,end_time):
+            start = datetime.fromisoformat(busy_time['start'])
+            end = datetime.fromisoformat(busy_time['end'])
+            busy_times.append((start, end))
+    compute_free_times(busy_times,start_time,end_time)
+    
